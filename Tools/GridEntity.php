a78f0cdb9b62b066f41ae47985aab85fac685ab6
houcemL <houcem.lamari@gmail.com>
2014-06-29 19:18:26 +0100

    Automatic fetch and load

--- Tools/GridEntity.php
+++ Tools/GridEntity.php
@@ -23,9 +23,10 @@ class GridEntity {
     protected $alias;
     protected $fields;
     protected $em;
-
+    protected $router;
     public function __construct(ContainerInterface $container) {
         $this->em = $container->get('doctrine.orm.entity_manager');
+        $this->router = $container->get('router');
     }
 
     public function setEntityClass($class) {
@@ -43,11 +44,54 @@ class GridEntity {
     public function EntitiesConf() {
         return $this->em->getConfiguration();
     }
-    public function EntityFetchData($class) {
-        
+    /**
+     * 
+     * @param type $class
+     * @return \Sf\GridLBundle\Model\jqdata
+     */
+    public function EntitiesFetchData($class) {
+        $data = new jqdata();
+        $entities = $this->em->getRepository($class)->findAll();
+        foreach ($entities as $entity) {
+            $row = new \stdClass;
+            $row->id = $entity->getId();
+            $cell = $this->mappedToArray($class, $entity);
+            $row->cell = $cell;
+            $data->rows[] = $row;
+        }
+        return $data;
     }
-
-    public function gridByDoctrineEntity($class) {
+    /**
+     * 
+     * @param type $class
+     * @param type $entity
+     * @return type
+     */
+    public function mappedToArray($class, $entity) {
+        $meta = $this->EntityFields($class);
+        $fieldsMap = $meta->fieldMappings;
+        $cells = array();
+        foreach ($fieldsMap as $key => $field) {
+            $first = strtoupper(substr($key, 0, 1));
+            $rest = substr($key, 1);
+            $getter = "get" . $first . $rest;
+            $keyvalue = $entity->$getter();
+            $cells[] = $keyvalue;
+        }
+        return $cells;
+    }
+    /**
+     * 
+     * @param type $class : format of the class "NamebundleBundle:className"
+     * @param \Sf\GridLBundle\Model\jqGridConfig $grid
+     * @param Boolean $load : if true it generate an absolute url to load data
+     *  automatically
+     * @return \Sf\GridLBundle\Model\jqGridConfig
+     */
+    public function gridByDoctrineEntity($class, jqGridConfig $grid, $load) {
+        if ($load) {
+            $grid->url = $this->router->generate("gridL_load", array(), true);
+        }
         $grid = new jqGridConfig;
         $meta = $this->EntityFields($class);
         $fields = $meta->fieldNames;
@@ -65,15 +109,7 @@ class GridEntity {
         $grid->colModel = $col;
         $grid->colNames = $names;
         return $grid;
-//        return array(
-//                "grid" => $grid,
-//                "id" => "id",
-//                "saveId" => "saveId",
-//                "restoreId" => "restoreId",
-//                "editId" => "editId"
-//            );
     }
 
-    //put your code here
 }
 
