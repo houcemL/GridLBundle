cb8da02286715c02974fd00f075952146efbe301
houcemL <houcem.lamari@gmail.com>
2014-07-17 07:45:22 +0100

    handle subgrid and pagination

--- Model/jqGridConfig.php
+++ Model/jqGridConfig.php
@@ -14,28 +14,41 @@ namespace Lamari\GridLBundle\Model;
  */
 class jqGridConfig {
 
+    public $pager;
     public $datatype;
     public $url;
     public $colNames;
     public $colModel;
     public $method;
-    public $onEvents;
-//    public $rowNum;
-//    public $rowList;
-//    public $pager;
-//    public $sortname;
-//    public $viewrecords;
-//    public $sortorder;
+    //public $onEvents;
+    public $rowNum;
+    public $rowList;
+    public $sortname;
+    public $viewrecords;
+    public $sortorder;
     public $caption;
-    public $editurl;
+    public $height;
+//    public $editurl;
     public $subGrid;
+
+    // public $events;
+    /**
+     * 
+     * @param type $conf
+     */
     public function __construct($conf = array()) {
+        $this->height = empty($conf["height"]) ? "auto" : $conf["height"];
         $this->caption = empty($conf["caption"]) ? "caption" : $conf["caption"];
         $this->subGrid = empty($conf["subGrid"]) ? false : $conf["subGrid"];
         $this->method = empty($conf["method"]) ? "get" : $conf["method"];
         $this->url = empty($conf["url"]) ? "load" : $conf["url"];
         $this->datatype = empty($conf["datatype"]) ? "json" : $conf["datatype"];
-        $this->onEvents = "";
+        $this->pager = empty($conf["pager"]) ? "pagerL" : $conf["pager"];
+        $this->sortorder = empty($conf["sortorder"]) ? "desc" : $conf["sortorder"];
+        $this->sortname = empty($conf["sortname"]) ? "id" : $conf["sortname"];
+        $this->rowNum = empty($conf["rowNum"]) ? "3" : $conf["rowNum"];
+        $this->rowList = empty($conf["rowList"]) ? array(5, 10, 15) : $conf["rowList"];
+        $this->viewrecords = empty($conf["viewrecords"]) ? true : $conf["viewrecords"];
     }
 
     /**
@@ -61,20 +74,19 @@ class jqGridConfig {
         if (is_array($models)) {
             foreach ($models as $model) {
                 if (!is_a($model, "jqCol")) {
-                    throw new Exception("jqGrid column model's not configured !");
+                    throw new Exception("jqGrid column model's not confiured !");
                 }
             }
             $this->colModel = $models;
         }
         return false;
     }
-    
+
     /**
      * get the column's names.
      */
     public function getColModel() {
-        return $this->colModels;
+        return $this->colModel;
     }
 
-    //put your code here
 }
